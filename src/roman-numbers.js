659b904e702c063d747fdf8db7a649ca3118314e
Leonie Hein <hein.leonie@gmx.net>
2022-07-11 12:53:30 +0200

    Aufgabe b erledigt

---
+++ src/roman-numbers.js
@@ -0,0 +1,8 @@
+
+function convertToRoman(){
+    if (1 == "I"){
+        return "I";
+    }
+}
+
+module.exports = {convertToRoman}
--- src/roman-numbers.spec.js
+++ src/roman-numbers.spec.js
@@ -1,7 +1,13 @@
+const {convertToRoman}= require ('./roman-numbers')
 describe('roman_numbers', () => {
     
     // Aufgabe a
     it('should work', () => {
        console.log("Hallo Ansbach");
        })
+
+    it('should return I', () => {
+        console.log(convertToRoman(1));
+    })
+
     })
