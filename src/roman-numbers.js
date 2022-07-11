fba023f1dc04e80f307f1936747d8f1fc51b46d3
Leonie Hein <hein.leonie@gmx.net>
2022-07-11 14:16:11 +0200

    Aufgabe c erledigt

--- src/roman-numbers.js
+++ src/roman-numbers.js
@@ -1,8 +1,23 @@
+function convertToRoman(num) {
+  var romanNum = {
+    III: 3,
+    II: 2,
+    I: 1,
+  };
+  var roman = "";
 
-function convertToRoman(num){
-    if (num == 1){
-        return "I";
+  //vergleicht, ob Eingabe >= 1 ist, anschließend ob die Eingabe einer Zahl aus romanNum entspricht. Durchrechnen der Zahl mit anschließender Ausgabe
+  for (var key in romanNum) {
+    if (num >= 1) {
+      while (num >= romanNum[key]) {
+        roman += key;
+        num -= romanNum[key];
+        return roman;
+      }
+    } else {
+      return "ungültige Zahl";
     }
+  }
 }
 
-module.exports = {convertToRoman}
+module.exports = { convertToRoman };
